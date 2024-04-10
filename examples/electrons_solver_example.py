2e5e57728206da7dbe65bf0e80391a06d88ac394
vincent-maillou <vmaillou@iis.ee.ethz.ch>
2024-04-10 18:54:08 +0200

    frst setup commit

---
+++ examples/electrons_solver_example.py
@@ -0,0 +1,1 @@
+# Copyright 2023-2024 ETH Zurich and QuaTrEx authors. All rights reserved.
---
+++ src/quatrex/__about__.py
@@ -0,0 +1,2 @@
+# Copyright 2023-2024 ETH Zurich and QuaTrEx authors. All rights reserved.
+__version__ = "0.0.1"
